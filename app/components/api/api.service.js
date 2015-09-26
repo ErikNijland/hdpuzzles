b272f00e66fae50dfcdb8a82210eafff16c097d5
Erik Nijland <e.n.nijland@gmail.com>
2015-09-26 20:57:06 +0200

    simple placeholder css for homepage

--- app/components/api/api.service.js
+++ app/components/api/api.service.js
@@ -45,6 +45,36 @@
             }, {
                 "id": 10,
                 "filename": "10.jpg"
+            }, {
+                "id": 1,
+                "filename": "1.jpg"
+            }, {
+                "id": 2,
+                "filename": "2.jpg"
+            }, {
+                "id": 3,
+                "filename": "3.jpg"
+            }, {
+                "id": 4,
+                "filename": "4.jpg"
+            }, {
+                "id": 5,
+                "filename": "5.jpg"
+            }, {
+                "id": 6,
+                "filename": "6.jpg"
+            }, {
+                "id": 7,
+                "filename": "7.jpg"
+            }, {
+                "id": 8,
+                "filename": "8.jpg"
+            }, {
+                "id": 9,
+                "filename": "9.jpg"
+            }, {
+                "id": 10,
+                "filename": "10.jpg"
             }]);
 
             return d.promise;
