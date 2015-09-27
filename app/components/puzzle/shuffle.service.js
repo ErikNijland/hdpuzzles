e3cbb2e43ffbcf0014d714a40cabb2662739ba72
Erik Nijland <e.n.nijland@gmail.com>
2015-09-27 16:42:27 +0200

    shuffle service

---
+++ app/components/puzzle/shuffle.service.js
@@ -0,0 +1,29 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('hdpuzzles')
+        .service('shuffle', shuffleService);
+
+    function shuffleService () {
+        return {
+            "shuffle": shuffle
+        };
+
+        function shuffle (input) {
+            var currentIndex = input.length;
+
+            while (--currentIndex) {
+                var randomIndex = Math.floor(Math.random() * (currentIndex + 1)),
+                    temp;
+
+                temp = input[currentIndex];
+
+                input[currentIndex] = input[randomIndex];
+                input[randomIndex] = input[temp];
+            }
+
+            return input;
+        }
+    }
+})();
