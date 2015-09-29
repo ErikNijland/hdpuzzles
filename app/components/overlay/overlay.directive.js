5c2b56768c75d59511654a39eaee2aa3f76bcc84
Erik Nijland <e.n.nijland@gmail.com>
2015-09-29 21:16:20 +0200

    Added an overlay component.

---
+++ app/components/overlay/overlay.directive.js
@@ -0,0 +1,15 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('hdpuzzles')
+        .directive('overlay', overlayDirective);
+
+    function overlayDirective () {
+        return {
+            restrict: 'A',
+            transclude: true,
+            templateUrl: 'overlay/overlay.html'
+        };
+    }
+})();
