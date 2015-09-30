647753c211031354d2cf1486fca676e5de9d4175
Erik Nijland <e.n.nijland@gmail.com>
2015-09-30 22:16:17 +0200

    The canvas preview is now responsive and centered.

--- app/components/puzzle/canvas.directive.js
+++ app/components/puzzle/canvas.directive.js
@@ -5,63 +5,88 @@
         .module('hdpuzzles')
         .directive('hdpuzzlesCanvas', hdpuzzlesCanvasDirective);
 
-    hdpuzzlesCanvasDirective.$inject = ['$window', '$document', '$timeout'];
+    hdpuzzlesCanvasDirective.$inject = ['difficultySettings'];
 
-    function hdpuzzlesCanvasDirective ($window, $document, $timeout) {
+    function hdpuzzlesCanvasDirective (difficultySettings) {
         return {
             scope: {
                 image: '@',
                 difficulty: '@',
                 preview: '='
             },
-            //replace: true,
             link: function (scope, element) {
                 var availableWidth,
                     availableHeight,
                     canvasWidth,
                     canvasHeight,
+                    canvasOffsetX,
+                    canvasOffsetY,
                     image,
                     canvas,
                     context;
 
-                canvas = element.find('canvas')[0];
-                context = canvas.getContext('2d');
+                initialize();
 
-                calculateDimensions();
+                function initialize () {
+                    canvas = element.find('canvas')[0];
+                    context = canvas.getContext('2d');
 
-                image = new $window.Image();
-                image.src = scope.image;
-                image.onload = render;
+                    image = new window.Image();
+                    image.src = scope.image;
+                    image.onload = function () {
+                        calculateDimensions();
+                        render();
+
+                        angular.element(window).on('resize', function () {
+                            calculateDimensions();
+                            render();
+                        });
+                    };
+                }
 
                 function calculateDimensions () {
-                    console.log(canvas.clientWidth);
-                    console.log(canvas.clientHeight);
-                    //console.log(angular.element($document.querySelectorAll('.puzzle__canvas')[0]).clientHeight);
+                    var containerDiv,
+                        minimumPadding = 2,
+                        imageResizeRatio;
 
-                    availableWidth = 1425;
-                    availableHeight = 605;
-                    canvasWidth = 1425;
-                    canvasHeight = 605;
+                    containerDiv = document.querySelector('.puzzle__canvas');
 
-                    scope.canvasWidth = canvasWidth;
-                    scope.canvasHeight = canvasHeight;
+                    availableWidth = containerDiv.clientWidth;
+                    availableHeight = containerDiv.clientHeight;
+
+                    imageResizeRatio = Math.max(
+                        1,
+                        image.width / (availableWidth - minimumPadding * 2),
+                        image.height / (availableHeight - minimumPadding * 2)
+                    );
+
+                    if (scope.preview) {
+                        canvasWidth = Math.floor(image.width / imageResizeRatio);
+                        canvasHeight = Math.floor(image.height / imageResizeRatio);
+                    } else {
+                        //Todo: calculation based on number of columns and rows
+                    }
+
+                    canvasOffsetX = Math.round((availableWidth - canvasWidth) / 2);
+                    canvasOffsetY = Math.round((availableHeight - canvasHeight) / 2);
+
+                    angular.element(canvas).prop('width', canvasWidth);
+                    angular.element(canvas).prop('height', canvasHeight);
+
+                    angular.element(canvas).css('top', canvasOffsetY + 'px');
+                    angular.element(canvas).css('left', canvasOffsetX + 'px');
                 }
 
                 function render () {
                     if (scope.preview) {
                         //Draw the complete image
-                        context.drawImage(image, 0, 0, image.width, image.height, 0, 0, scope.canvasWidth, scope.canvasHeight);
+                        context.drawImage(image, 0, 0, image.width, image.height, 0, 0, canvasWidth, canvasHeight);
                     } else {
                         //Draw the puzzle pieces
                     }
                 }
-
-                angular.element($window).on('resize', function () {
-                    calculateDimensions();
-                    render();
-                });
             },
-            template: '<canvas></canvas>'
+            templateUrl: 'puzzle/puzzle-canvas.html'
         };
     }
 })();
