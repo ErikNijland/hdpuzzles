1991fd9561c09e186dd4d43fa82545dd3a0dde25
Erik Nijland <e.n.nijland@gmail.com>
2015-11-13 19:47:55 +0100

    Added compression

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express'),
+    compression = require('compression'),
     app = express();
 
 app.set('port', process.env.PORT || 3000);
 
+app.use(compression());
 app.use(express.static(__dirname + '/public'));
 
 app.listen(app.get('port'));
