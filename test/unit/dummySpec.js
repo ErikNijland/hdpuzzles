225ddcb85c0637a586b72388996f37fe300db62b
Erik Nijland <e.n.nijland@gmail.com>
2015-11-20 22:58:39 +0100

    Test if Travis CI reports a failure for failing the jshint check.

--- test/unit/dummySpec.js
+++ test/unit/dummySpec.js
@@ -2,6 +2,6 @@ describe('Test stuff', function () {
    it('should just work', function () {
        angular.mock.module('hdpuzzles');
 
-       expect(true).toBe(false);
+       expect(true).toBe(true);
    });
 });
