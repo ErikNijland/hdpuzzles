ce0fcfdd33d5aadf4622a7e73916e9092c6da1ae
Erik Nijland <e.n.nijland@gmail.com>
2016-01-03 19:54:29 +0100

    switch back to s3

--- app/components/s3/s3.constants.js
+++ app/components/s3/s3.constants.js
@@ -4,9 +4,9 @@
     angular
         .module('hdpuzzles')
         .constant('s3', {
-            //"IMAGE_LOCATION": "https://hdpuzzles.s3.eu-central-1.amazonaws.com/puzzles/",
-            //"THUMBNAIL_LOCATION": "https://hdpuzzles.s3.eu-central-1.amazonaws.com/thumbnails/"
-            "IMAGE_LOCATION": "http://hdpuzzles.com/",
-            "THUMBNAIL_LOCATION": "http://hdpuzzles.com/thumbnails/"
+            "IMAGE_LOCATION": "https://hdpuzzles.s3.eu-central-1.amazonaws.com/puzzles/",
+            "THUMBNAIL_LOCATION": "https://hdpuzzles.s3.eu-central-1.amazonaws.com/thumbnails/"
+            //"IMAGE_LOCATION": "http://hdpuzzles.com/",
+            //"THUMBNAIL_LOCATION": "http://hdpuzzles.com/thumbnails/"
         });
 })();
